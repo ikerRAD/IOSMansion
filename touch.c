4252792a4590ae0db87273802ae04b49e676a70d
saragracia8 <80322107+saragracia8@users.noreply.github.com>
2021-03-16 11:53:07 +0100

    Update touch.c

--- touch.c
+++ touch.c
@@ -1,26 +1,22 @@
-﻿//touch
-
-
-//https://www.geeksforgeeks.org/touch-command-in-linux-with-examples/ 
-
-
-
-
 #include <fcntl.h>
 #include <stdlib.h>
 
 
 int main(int argc, char* argv[])
 {
+        int fd;
+        
         if (argc <2)  {
                 write(2, "Usage: touch file_name\n", 24);
                       exit(1);
         }
 
-
-        if ((fd = open(argv[1], O_CREAT, 0666)) == -1) {
+        int i;
+        for (i=1; i<argc; i++) {
+                if ((fd = open(argv[i], O_CREAT, 0666)) == -1) {
                       write(2, "ERROR open\n", 11);
                       exit(1);
-          }
+                }
+        }
         close(fd);
 }
