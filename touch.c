d7ee2fa8dd9b1c1dfae0e37da3de9097e9f53cda
saragracia8 <80322107+saragracia8@users.noreply.github.com>
2021-03-16 11:34:27 +0100

    Add files via upload

---
+++ touch.c
@@ -0,0 +1,26 @@
+﻿//touch
+
+
+//https://www.geeksforgeeks.org/touch-command-in-linux-with-examples/ 
+
+
+
+
+#include <fcntl.h>
+#include <stdlib.h>
+
+
+int main(int argc, char* argv[])
+{
+        if (argc <2)  {
+                write(2, "Usage: touch file_name\n", 24);
+                      exit(1);
+        }
+
+
+        if ((fd = open(argv[1], O_CREAT, 0666)) == -1) {
+                      write(2, "ERROR open\n", 11);
+                      exit(1);
+          }
+        close(fd);
+}
