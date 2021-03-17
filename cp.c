2d236fefa36ec07e72d3c9869b601b436685106e
saragracia8 <80322107+saragracia8@users.noreply.github.com>
2021-03-17 11:25:37 +0100

    Create cp.c

---
+++ cp.c
@@ -0,0 +1,18 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+        int e;
+        
+        if (argc !=3)  {
+                write(2, "Usage: cp file_name copy_name\n", 24);
+                      exit(1);
+        }
+        
+        int e = link(argv[1], argv[2]);
+        if (e=-1) {
+          write(2, "ERROR copy\n", 11);
+        }
+}
