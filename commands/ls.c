d5f0ae05beda883bfef537e4956aedb1bfd34c66
ztira <72011798+ztira@users.noreply.github.com>
2021-03-23 12:06:45 +0100

    Update ls.c

--- commands/ls.c
+++ commands/ls.c
@@ -4,12 +4,15 @@
 #include <stdlib.h>
 void _ls(const char *dir){
         struct dirent *d;
+	//Open the the directory given
         DIR *directorio = opendir(dir);
 	char *str;
+	//See if the directory exists
         if(!directorio){
                 write(2, "ERROR open\n", 11);
                 exit(1);
         }
+	  //Print at the standar output the entries from the directory that doesn't start with a dot.
           while((d = readdir(directorio)) != NULL) {
              if(d->d_name[0]=='.')
               continue;
@@ -19,6 +22,7 @@ void _ls(const char *dir){
            }
 	   
 }
+//Call the program above with current directory
 int main(int argc, const char *argv[]){
   _ls(".");
 }
