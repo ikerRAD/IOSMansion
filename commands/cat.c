411fba3dcbee8929e8008a1d2d96c232f529a7f1
ztira <72011798+ztira@users.noreply.github.com>
2021-03-23 11:29:08 +0100

    Update cat.c

--- commands/cat.c
+++ commands/cat.c
@@ -9,24 +9,21 @@ int main(int argc, char* argv[])
    const char* c;
    int length;
    int state;
-	   
+   //Enable process abortion	   
    signal(2, SIG_DFL);
-
+   //In case that the user doesn't give an file name to read argc is going to be 1	
    if(argc == 1){
+      //Whatever is write in the standar input is going to be print in the standar output
       while (state = read(0, &c, 1) > 0) {
-	 if(strcmp(c, "")==0){
-            //write(1, "Empty", strlen("Empty"));
-            //exit(1);
-         }
          write(1, &c, 1);
       } 
-
    } else {
+      //fd will become -1 in case that the user gives a file name that doesn't exist 
       if ((fd = open(argv[1], O_RDONLY)) == -1) {
          write(2, "ERROR open\n", 11);
          exit(1);
       }
-
+      //The file will be print completely in the standar output
       if(length=read(fd,&c, 1)>0){
          while (length > 0) {
             write(1,&c, length);
