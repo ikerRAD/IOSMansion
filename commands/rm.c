2ea0e0318a4ff9e80e9be308d67a6dd9c0a548da
ztira <72011798+ztira@users.noreply.github.com>
2021-03-23 11:58:38 +0100

    Update rm.c

--- commands/rm.c
+++ commands/rm.c
@@ -8,14 +8,18 @@ int main(int argc, char* argv[])
         int e;
         char str;
         
+        //Avoid not having any file name
         if (argc <2)  {
                 write(2, "Usage: rm file_name\n", 24);
                 exit(1);
         }
         
         int i;
+        //Delete all the files from the names given
         for (i=1; i<argc; i++) {
+                //Remove the file
                 e=unlink(argv[i]);
+                //See if any error happened
                 if (e==-1) {
                      write(2, "ERROR remove argument number ", 29);
                      write(2, &i, sizeof(i));
