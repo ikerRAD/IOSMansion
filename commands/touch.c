08728ead34fa12e4174328e2472a585af0b39049
ztira <72011798+ztira@users.noreply.github.com>
2021-03-23 11:51:28 +0100

    Update touch.c

--- commands/touch.c
+++ commands/touch.c
@@ -5,14 +5,16 @@
 int main(int argc, char* argv[])
 {
         int fd;
-        
+        //To avoid not having a file name
         if (argc <2)  {
                 write(2, "Usage: touch file_name\n", 24);
                       exit(1);
         }
 
         int i;
+        //Creates all the files with the names given
         for (i=1; i<argc; i++) {
+                //Sees if there is any problem in the creation
                 if ((fd = open(argv[i], O_CREAT, 0666)) == -1) {
                       write(2, "ERROR open\n", 11);
                       exit(1);
