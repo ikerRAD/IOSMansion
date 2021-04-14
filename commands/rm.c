ac9aecad3f548175f930a73fe97b255c411541c1
saragracia8 <80322107+saragracia8@users.noreply.github.com>
2021-04-14 11:14:35 +0200

    Update rm.c

--- commands/rm.c
+++ commands/rm.c
@@ -21,6 +21,25 @@ int main(int argc, char* argv[])
                 e=unlink(argv[i]);
                 //See if any error happened
                 if (e==-1) {
+                        switch (errno) {
+                                case EACCES:
+                                        write(2, "You have not the permission to remove the file: ",strlen("The access to the directory containing "));
+                                        write(2, &argv[i], sizeof(argv[i]));
+                                        write(2, "\n", strlen("\n"));
+                                        break;
+                                case EBUSY:
+                                        write(2, "The file ", strlen("The file "));
+                                        write(2, &argv[i], sizeof(argv[i]));
+                                        write(2, " can't be deleted because it is being used by another process\n", strlen(" can't be deleted because it is being used by another process\n"));
+                                        break;
+                                case EISDIR:
+                                        write(2, &argv[i], sizeof(argv[i]));
+                                        write(2, " is a directory, can't be deleted\n", strlen(" is a directory, can't be deleted\n"));
+                                        break;
+                                       
+                                        
+                               
+                        }
                      write(2, "ERROR remove argument number ", 29);
                      write(2, &i, sizeof(i));
                      write(2, "\n",1);
