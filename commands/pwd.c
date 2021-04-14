a26b9ace9c9ba97d541b15c667a40877a7ae9468
saragracia8 <80322107+saragracia8@users.noreply.github.com>
2021-04-14 11:24:27 +0200

    Update pwd.c

--- commands/pwd.c
+++ commands/pwd.c
@@ -7,12 +7,17 @@ int main(int argc, char* argv[])
    int fd;
    char c;
    int length;
-   //fd will be -1 if the .description file doesn't exist
-   if ((fd = open(".description", O_RDONLY)) == -1) {
-      write(2, "ERROR open\n", 11);
+   //The command structure is incorret
+   if (argc!=1) {
+      write(2, "Usage: pwd\n", strlen("Usage: pwd\n"));
       exit(1);
    }
-   //print .descrption file at the standar output
+   //fd will be -1 if the .description file doesn't exist
+   if ((fd = open(".description", O_RDONLY)) == -1) {
+      write(2, "An error has occurred with the open system call\n", strlen("An error has occurred with the open system call\n"));
+      exit(1);
+   }
+   //print .description file at the standar output
    while (length=read(fd, &c, 1) > 0) {
       write(1,&c, length);
    }
