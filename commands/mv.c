ce16bf664b5772ad6c4bf9b8dbdf86274e2a8430
saragracia8 <80322107+saragracia8@users.noreply.github.com>
2021-04-20 12:15:26 +0200

    Update mv.c

--- commands/mv.c
+++ commands/mv.c
@@ -6,13 +6,19 @@ int main(int argc, char* argv[])
 {
         int e;
         
+        
         //To ensure that the command follows the mv structure.
         if (argc !=3)  {
                 write(2, "Usage: mv file_name new_location\n", 24);
                       exit(1);
         }
         //Copy the file.
-        e = link(argv[1], argv[2]);
+      
+        char path[10] = "./";
+        strcat(path, argv[2]);
+        strcat(path,"/");
+        strcat(path,argv[1]);
+        e = link(argv[1], path);
         if (e==-1) {
           write(2, "ERROR copy\n", 11);
           exit(1);
