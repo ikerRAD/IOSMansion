c5fb92d245b2ce3ebc558b21c5dcae9ed4e6ecb6
saragracia8 <80322107+saragracia8@users.noreply.github.com>
2021-04-22 22:11:42 +0200

    Update cat.c

--- commands/cat.c
+++ commands/cat.c
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
    signal(2, SIG_DFL);
    //In case that the user doesn't give a file name to read, argc is going to be 1	
    if(argc == 1){
-      //Whatever is written on the standar input is going to be printed on the standard output
+      //Whatever is written on the standard input is going to be printed on the standard output
       while (state = read(0, &c, 1) > 0) {
          write(1, &c, 1);
       } 
@@ -22,10 +22,19 @@ int main(int argc, char* argv[])
       //fd will become -1 in case that the user gives a file name that doesn't exist 
       if ((fd = open(argv[1], O_RDONLY)) == -1) {
          switch (errno) {
-               case 
-         exit(1);
+		   	case ENOENT:
+			    	write(2, "Some locations in the path or the object specified doesn't exist\n", strlen("Some locations in the path or the object specified doesn't exist\n");
+				   break;
+			   case ENOTDIR:
+				   write(2, "A component of the path isn't a location\n", strlen("A component of the path isn't a location\n"));
+               break;
+			   default:
+				   write(2, "An error has occurred\n", strlen("An error has occurred\n"));  
+				   break;
+	      }
+	    exit(1);
       }
-      //The file will be printed completely in the standar output
+      //The file will be printed completely on the standard output
       if(length=read(fd,&c, 1)>0){
          while (length > 0) {
             write(1,&c, length);
