8b6983556150f2d0faefad3db8bfaa8e4bd33878
saragracia8 <80322107+saragracia8@users.noreply.github.com>
2021-04-22 20:08:20 +0200

    Update cp.c

--- commands/cp.c
+++ commands/cp.c
@@ -21,7 +21,7 @@ int main(int argc, char* argv[])
                              write(2, "There's already an object with that name in the location\n",strlen("There's already an object with that name in the location\n"));      
                              break;         
                         case ENOENT:
-                             write(2, "Some locations in the path don't exist\n",strlen("Some locations in the path don't exist\n"));      
+                             write(2, "Some locations in the path or the file specified doesn't exist\n",strlen("Some locations in the path or the file specified doesn't exist\n"));      
                              break;
                         case ENOTDIR:
                              write(2, "A component of the path isn't a location\n", strlen("A component of the file isn't a location\n"));
