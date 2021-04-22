1f63d818e882e91f843fa69ed41954e49292fb19
saragracia8 <80322107+saragracia8@users.noreply.github.com>
2021-04-22 21:44:54 +0200

    Update pwd.c

--- commands/pwd.c
+++ commands/pwd.c
@@ -12,27 +12,26 @@ int main(int argc, char* argv[])
    char cwd[PATH_MAX];
    char *path;
    
-   //The command structure is incorret
+   //The command structure is incorrect
    if (argc!=1) {
       write(2, "Usage: pwd\n", strlen("Usage: pwd\n"));
       exit(1);
    }
    
-   if (getcwd(cwd, sizeof(cwd)) != NULL) {
+   //get current working directory and print the path
+   if (getcwd(cwd, sizeof(cwd)) != NULL) { //it is not expected that it will give any error
 	path = strstr(cwd, "/nowhere");
-       write(1,"Location: ", strlen("Location: "));
+        write(1,"Location: ", strlen("Location: "));
 	write(1, path, strlen(path));
 	write(1, "\n", strlen("\n"));
-   } else {
-       perror("getcwd() error");
-       exit(1);
    }
    
    //fd will be -1 if the .description file doesn't exist
-   if ((fd = open(".description", O_RDONLY)) == -1) {
-      write(2, "An error has occurred with the open system call\n", strlen("An error has occurred with the open system call\n"));
+   if ((fd = open(".description", O_RDONLY)) == -1) { //it won't give any error, because .description is present in every location
+      write(2, "An error has occurred\n", strlen("An error has occurred\n"));
       exit(1);
    }
+	
    //print .description file at the standar output
    while (length=read(fd, &c, 1) > 0) {
       write(1,&c, length);
