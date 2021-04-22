9c0a4e34f268d23b6bc7140b4e6d636390cb7c97
saragracia8 <80322107+saragracia8@users.noreply.github.com>
2021-04-22 21:54:47 +0200

    Update cat.c

--- commands/cat.c
+++ commands/cat.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <errno.h>
 
 int main(int argc, char* argv[])
 {
@@ -11,16 +12,17 @@ int main(int argc, char* argv[])
    int state;
    //Enable process abortion	   
    signal(2, SIG_DFL);
-   //In case that the user doesn't give an file name to read argc is going to be 1	
+   //In case that the user doesn't give a file name to read, argc is going to be 1	
    if(argc == 1){
-      //Whatever is written in the standar input is going to be print in the standar output
+      //Whatever is written on the standar input is going to be printed on the standard output
       while (state = read(0, &c, 1) > 0) {
          write(1, &c, 1);
       } 
-   } else {
+   } else if (argc == 2) {
       //fd will become -1 in case that the user gives a file name that doesn't exist 
       if ((fd = open(argv[1], O_RDONLY)) == -1) {
-         write(2, "ERROR open\n", 11);
+         switch (errno) {
+               case 
          exit(1);
       }
       //The file will be printed completely in the standar output
@@ -31,6 +33,10 @@ int main(int argc, char* argv[])
          }
          write(1, "\n", strlen("\n"));
       }
+      close(fd);
+   } else {
+      //Command structure is not correct
+      write(2, "Usage: cat [file_name]\n", strlen("Usage: cat [file_name]\n"));
    }
-   close(fd);
+   
 }
