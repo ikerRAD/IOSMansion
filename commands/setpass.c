36cdcb51103fdf66bbd0c7a2be06b1563a514a32
ikerRAD <63415322+ikerRAD@users.noreply.github.com>
2021-05-01 00:07:43 +0200

    setpwd command

--- commands/setpass.c
+++ commands/setpass.c
@@ -15,10 +15,39 @@ int main(int argc, char* argv[])
         write(2, "You set a password to objects from outside your location\n", strlen("You set a password to objects from outside your location\n"));
         exit(1);
     }
+
+    int found=0;
+    //search for .pass[location] file
+    struct dirent *d;
+    char path[10] = "./";
+    strcat(path,argv[1]);
+
+    DIR *direc=opendir(".");
+
+    char *str;
+
+    while((d=readdir(direc))!=NULL && found!=1){
+        str=d->d_name;
+        if(strcmp(str,argv[1])==0) found=1;
+    }
+    closedir(direc);
+
+    if(found==0){
+        write(2, "You must select an existing directory in this location\n", strlen("You must select an existing directory in this location\n"));
+        exit(1);
+    }
+
+    if(strcmp(argv[1],"library")){
+        write(2, "Do not set a password there!\n", strlen("Do not set a password there!\n"));
+        exit(1);
+    }
+
     int i;
 
     //Sees if there is any problem in the creation
-    if ((fd = open(argv[1], O_CREAT, 0666)) < 0) {
+    char file[20]=".pass";
+    strcat(file,argv[1]);
+    if ((fd = open(file, O_CREAT, 0666)) < 0) {
          switch (errno) {
              case ENOENT:
                  write(2, "Some locations in the path don't exist\n",strlen("Some locations in the path don't exist\n"));
@@ -33,7 +62,7 @@ int main(int argc, char* argv[])
          exit(1);
     }
 
-    write(fd,argv[2],strlen(argv[2]));
+    write(fd,&argv[2],strlen(argv[2]));
 
     close(fd);
 
