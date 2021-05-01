7d69c329fe0485cc066d73c004ad564ca41a2b87
acaf0222 <acaf0222@dif-linuxserver.ehu.es>
2021-05-01 20:55:08 +0200

    bug corrected

--- myshell.c
+++ myshell.c
@@ -142,61 +142,61 @@ void change_permissions (char *cwd) {
 		chmod(mv, S_IRUSR|S_IRGRP|S_IROTH);
 		chmod(cp, S_IRUSR|S_IRGRP|S_IROTH);
 		chmod(grep, S_IRUSR|S_IRGRP|S_IROTH);
-		chmod(man, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
-		chmod(help, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(man, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(help, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
 		chmod(rm, S_IRUSR|S_IRGRP|S_IROTH);
 		chmod(cook, S_IRUSR|S_IRGRP|S_IROTH);
 		chmod(brush, S_IRUSR|S_IRGRP|S_IROTH);
 	} else if (strcmp(dirpath,"/wardrobe")==0) {
-		chmod(touch, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(touch, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
 		chmod(mv, S_IRUSR|S_IRGRP|S_IROTH);
 		chmod(cp, S_IRUSR|S_IRGRP|S_IROTH);
 		chmod(grep, S_IRUSR|S_IRGRP|S_IROTH);
-		chmod(man, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
-		chmod(help, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
-		chmod(rm, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(man, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(help, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(rm, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
 		chmod(cook, S_IRUSR|S_IRGRP|S_IROTH);
 		chmod(brush, S_IRUSR|S_IRGRP|S_IROTH);
 	} else if (strcmp(dirpath,"/library")==0) {
 		chmod(touch, S_IRUSR|S_IRGRP|S_IROTH);
 		chmod(mv, S_IRUSR|S_IRGRP|S_IROTH);
 		chmod(cp, S_IRUSR|S_IRGRP|S_IROTH);
-		chmod(grep, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
-		chmod(man, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
-		chmod(help, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(grep, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(man, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(help, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
 		chmod(rm, S_IRUSR|S_IRGRP|S_IROTH);
 		chmod(cook, S_IRUSR|S_IRGRP|S_IROTH);
 		chmod(brush, S_IRUSR|S_IRGRP|S_IROTH);
 	} else if (strcmp(dirpath,"/kitchen")==0) {
 		chmod(touch, S_IRUSR|S_IRGRP|S_IROTH);
-		chmod(mv, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(mv, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
 		chmod(cp, S_IRUSR|S_IRGRP|S_IROTH);
 		chmod(grep, S_IRUSR|S_IRGRP|S_IROTH);
-		chmod(man, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
-		chmod(help, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(man, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(help, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
 		chmod(rm, S_IRUSR|S_IRGRP|S_IROTH);
-		chmod(cook, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(cook, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
 		chmod(brush, S_IRUSR|S_IRGRP|S_IROTH);
 	} else if (strcmp(dirpath, "/oven")==0) {
 		chmod(touch, S_IRUSR|S_IRGRP|S_IROTH);
-		chmod(mv, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(mv, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
 		chmod(cp, S_IRUSR|S_IRGRP|S_IROTH);
 		chmod(grep, S_IRUSR|S_IRGRP|S_IROTH);
-		chmod(man, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
-		chmod(help, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(man, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(help, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
 		chmod(rm, S_IRUSR|S_IRGRP|S_IROTH);
 		chmod(cook, S_IRUSR|S_IRGRP|S_IROTH);
 		chmod(brush, S_IRUSR|S_IRGRP|S_IROTH);
 	} else if (strcmp(dirpath,"/bath")==0) {
-		chmod(touch, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(touch, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
 		chmod(mv, S_IRUSR|S_IRGRP|S_IROTH);
-		chmod(cp, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(cp, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
 		chmod(grep, S_IRUSR|S_IRGRP|S_IROTH);
-		chmod(man, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
-		chmod(help, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(man, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(help, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
 		chmod(rm, S_IRUSR|S_IRGRP|S_IROTH);
 		chmod(cook, S_IRUSR|S_IRGRP|S_IROTH);
-		chmod(brush, S_IRUSER | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
+		chmod(brush, S_IRUSR | S_IRGRP | S_IROTH| S_IXUSR |S_IXGRP  | S_IXOTH);
 	} 
 }
 
